18e40925e9d3db2736fcb6718fbf6799a6173609
Gustavo Xavier Pereira <6322875@univali.br>
2017-08-02 22:19:57 -0300

    Added POO - Java lessons

---
+++ POO - Java/code - lesson 1/br/univali/kob/poo1/aula1/Person.java
@@ -0,0 +1,56 @@
+package univali.kob.poo1.aula1;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Person {
+
+    /**
+     * Default constructor
+     */
+    public Person() {
+    }
+
+    /**
+     * Nome da Pessoa
+     */
+    private String name;
+
+    /**
+     * Data de nascimento da pessoa
+     */
+    private LocalDate dateOfBirth;
+
+    /**
+     * @return nome da pessoa
+     */
+    public String getName() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @param value
+     */
+    public void setName(String value) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public LocalDate getDateOfBirth() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param value
+     */
+    public void setDateOfBirth(LocalDate value) {
+        // TODO implement here
+    }
+
+}
