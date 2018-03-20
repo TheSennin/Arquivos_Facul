a7cbc8d88a7d58d0161f8a948ab69e322bbea221
TheSennin <gustavo2010tjsc@hotmail.com>
2018-03-19 23:28:11 -0300

    Roteiros 1 e 2 - POO Java

---
+++ POO Java/Roteiro 02 - Material do aluno/Roteiro 02 - Material do aluno/Meu Progresso/Aula02_Gustavo/src/br/univali/kob/poo1/aula02/Aula02.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.univali.kob.poo1.aula02;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Aula02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StudentInheritanceTest test = new StudentInheritanceTest();
+        test.run();
+    }
+}
---
+++ POO Java/Roteiro 02 - Material do aluno/Roteiro 02 - Material do aluno/Meu Progresso/Aula02_Gustavo/src/br/univali/kob/poo1/aula02/Person.java
@@ -0,0 +1,59 @@
+package br.univali.kob.poo1.aula02;
+
+import java.time.LocalDate;
+import java.time.Period;
+/**
+ *
+ */
+public class Person {
+
+    /**
+     * Default constructor
+     */
+    public Person() {
+    }
+
+    /**
+     * person's name
+     */
+    private String name;
+
+    /**
+     * Person's birthdate
+     */
+    private LocalDate dateOfBirth;
+
+    /**
+     * @return person's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param value person's name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return person's date of birth
+     */
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param value person's date of birth
+     */
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAge() {
+        Period period = null;
+        period = period.between(dateOfBirth, LocalDate.now());
+        return period.getYears();
+    }
+}
